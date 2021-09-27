9ebeb21d5ca426faa245c9b81b7c1caff807c544
SS <sagaranghan.stackapp@gmail.com>
2021-09-27 15:42:58 +0530

    Set screen orientation for application
    --> Set portrait mode for both android & IOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pedometer_demo/screen/pedometer_screen/pedometer_home.dart';
 
 void main() {
@@ -8,6 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       title: 'Flutter Pedometer Demo',
       debugShowCheckedModeBanner: false,
